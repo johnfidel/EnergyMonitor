c7501e8dbe0c1902a7eb7711ae3941156b6b67fd
johnfidel <afromann@gmx.ch>
2021-03-20 20:09:34 +0100

    added buildscript.

---
+++ build.sh
@@ -0,0 +1,10 @@
+RASPBERRY_IP=192.168.1.33
+TARGET_DIR=home/pi/bin/EnergyMonitor
+SSH_PASS=raspberry
+SSH_TARGET=pi@$RASPBERRY_IP
+ABSOLUTE_TARGET_DIR=$SSH_TARGET:/$TARGET_DIR
+
+sshpass -p $SSH_PASS ssh $SSH_TARGET sudo service EnergyMonitor stop
+dotnet publish -r linux-arm
+scp -r EnergyMonitor/bin/Debug/net5.0/linux-arm/publish/* $ABSOLUTE_TARGET_DIR
+sshpass -p $SSH_PASS ssh $SSH_TARGET sudo service EnergyMonitor start
