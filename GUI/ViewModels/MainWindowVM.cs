daa30f3da7afd6b8cecec7f91df9328331c42822
johnfidel <afromann@gmx.ch>
2021-10-08 22:09:58 +0200

    working on it

--- GUI/ViewModels/MainWindowVM.cs
+++ GUI/ViewModels/MainWindowVM.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using EnergyMonitor.BusinessLogic;
+using EnergyMonitor.Utils;
 
 namespace GUI.ViewModels {
   public class MainWindowVM : ViewModelBase {
@@ -8,5 +10,9 @@ namespace GUI.ViewModels {
 
     public double[] DataX { get; set; } = new double[] { 1, 2, 3, 4 };
     public double[] DataY { get; set; } = new double[] { 1, 2, 3, 4 };
+
+    public MainWindowVM() {
+      var state = Serializable.FromJson<State>(System.IO.File.ReadAllText(State.FILENAME));
+    }
   }
 }
